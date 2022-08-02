d4b83b5131deffed0c8c0321a7ee6d92007cc889
spazdrum <spazdrum@gmail.com>
2022-08-02 12:05:07 -0400

    Step 3. Further Tests

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,34 @@ class MessageApp {
   constructor() {
     this.messages = [];
   }
+
+  // post a message
+  post(content) {
+    let item = {
+      id: this.messages.length,
+      content: content,
+      date: new Date(),
+    };
+    this.messages.push(item);
+    return this.messages;
+  }
+
+  // get a message
+  get(id) {
+    return this.messages[id];
+  }
+
+  // update a message
+  update(id, update) {
+    this.messages[id].content = update;
+    return this.messages[id];
+  }
+
+  // delete a message
+  delete(id) {
+    this.messages.splice(id - 1, 1);
+    return this.messages;
+  }
 }
 
 export default MessageApp;
