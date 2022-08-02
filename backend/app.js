fc0b234d3a4180c31a7391d44fa23dcdc1f6aa65
spazdrum <spazdrum@gmail.com>
2022-08-02 12:16:00 -0400

    Step 4. Making Things Better

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,21 @@
+function newId(array) {
+  if (array.length > 0) {
+    return array[array.length - 1].id + 1;
+  } else {
+    return 1;
+  }
+}
+
 class MessageApp {
   constructor() {
     this.messages = [];
   }
 
   // post a message
-  post(content) {
+  post(message) {
     let item = {
-      id: this.messages.length,
-      content: content,
+      id: newId(this.messages),
+      content: message,
       date: new Date(),
     };
     this.messages.push(item);
@@ -16,18 +24,18 @@ class MessageApp {
 
   // get a message
   get(id) {
-    return this.messages[id];
+    return this.messages.filter((message) => message.id === id)[0];
   }
 
   // update a message
   update(id, update) {
-    this.messages[id].content = update;
-    return this.messages[id];
+    let index = this.messages.findIndex((message) => message.id === id);
+    this.messages[index].content = update;
   }
 
   // delete a message
   delete(id) {
-    this.messages.splice(id - 1, 1);
+    this.messages = this.messages.filter((message) => message.id != id);
     return this.messages;
   }
 }
